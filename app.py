c61524fc4ac2ff66e6508051c1e798f649c2dc82
qasim-mansoor <qasim.ali1609@gmail.com>
2023-10-10 17:45:33 +0400

    Added some Styling

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Qasim Mansoor',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2023'
+    },
+    {
+        'author': 'Sakine Mansoor',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2023'
+    }
+]
+
 @app.route("/")
-def hello():
-    return "<h1>Home Page</h1>"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts, title="Home")
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html", title='About')
 
 
 if __name__ == '__main__':
